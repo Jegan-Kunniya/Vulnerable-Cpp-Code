5f2642eda37aae76959094617a4157e99cdfb09f
Jegan Kunniya <jegan.kunniya@philips.com>
2025-09-11 00:51:07 +0530

    Update C++ standard from C++11 to C++17

--- Posix Stack Buffer Overflow/build.sh
+++ Posix Stack Buffer Overflow/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo bash -c 'echo 0 > /proc/sys/kernel/randomize_va_space'
-g++ webserver.cpp -o webserver -std=c++11 -lpistache -lpthread -fno-stack-protector
+g++ webserver.cpp -o webserver -std=c++17 -lpistache -lpthread -fno-stack-protector
